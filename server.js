c1fe9814e1e2bcd4546a9ad07413075b3481c9c4
DexterM <dextermunashemuwodzeri@gmail.com>
2022-02-11 12:58:19 +0200

    server add cloud db code

--- server.js
+++ server.js
@@ -3,13 +3,26 @@ const app = require('./app');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
-let constring = process.env.LOCAL_DB;
-const port = 3000;
+
+let constring = '';
+if(process.env.NODE_ENV==='development'){
+  constring = process.env.LOCAL_DB;
+}else if(process.env.NODE_ENV==='production'){
+  const dbString = process.env.CLOUD_DB;
+  constring = dbString.replace(
+    '<password>',
+    process.env.CLOUD_DB_PASSWORD
+  );
+
+}
+const port = process.env.PORT || 3000;
 
 console.log(`Connecting To DataBase.....`);
-mongoose.connect(constring, {
+mongoose.connect(constring, { 
     useNewUrlParser: true,
   }).then(
       app.listen(port,()=>{
     console.log(`Server started Succesfully. listening on port ${port}... `);
-}))
+})).catch(err=>{
+  console.log(err);
+})
